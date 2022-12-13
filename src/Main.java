0748129b9945c00fd149cbdf4c3ba77a0ff81836
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-13 16:37:59 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -15,8 +16,8 @@ public class Main {
     public static void task1() {
         int[] arr = generateRandomArray();
         int sum = 0;
-        for (int element : arr) {
-            sum += element;
+        for (int day : arr) {
+            sum += day;
         }
         String rouble = "я";
         if (sum % 10 > 4 || sum % 10 == 0) {
@@ -25,6 +26,21 @@ public class Main {
             rouble = "ь";
         }
 
-        System.out.printf("Сумма трат за месяц составила %d рубл%s", sum, rouble);
+        System.out.printf("Сумма трат за месяц составила %d рубл%s%n", sum, rouble);
     }
+
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int min = arr[0];
+        int max = arr[0];
+        for (int day : arr) {
+            if (day > max) {
+                max = day;
+            } else if (day < min) {
+                min = day;
+            }
+        }
+        System.out.printf("Минимальная сумма трат за день составила %d рублей. Максимальная сумма трат за день составила %d рублей", min, max);
+    }
+
 }
