b5bcb7fb5c6d00dee2dbc9a1aa7f46e5166ec95a
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-13 16:26:57 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,16 @@ public class Main {
     public static void task1() {
         int[] arr = generateRandomArray();
         int sum = 0;
-        for (int element:arr) {
+        for (int element : arr) {
             sum += element;
         }
-        System.out.printf("Сумма трат за месяц составила %d рублей", sum);
+        String rouble = "я";
+        if (sum % 10 > 4 || sum % 10 == 0) {
+            rouble = "ей";
+        } else if (sum % 10 == 1) {
+            rouble = "ь";
+        }
+
+        System.out.printf("Сумма трат за месяц составила %d рубл%s", sum, rouble);
     }
 }
